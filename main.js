c271d706ebe55ac735071bceed16964c5182d15d
Andre Schnyder Castello Branco <andrecb18@yahoo.com.br>
2023-11-06 21:20:39 -0800

    Solution

--- main.js
+++ main.js
@@ -4,7 +4,40 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+function handlelike () {
 
+  let like = document.querySelectorAll('.like-glyph')
+  like.forEach(element => element.addEventListener('click', (e)=>{
+    mimicServerCall()
+    .then ((res)=> {
+      if (res === 'Pretend remote server notified of action!'){
+        let heart = e.target
+        heart.classList.toggle('activated-heart')
+        if(heart.className === 'like-glyph activated-heart'){
+          heart.textContent = FULL_HEART 
+          } else {
+          heart.textContent = EMPTY_HEART
+        }
+      }
+    })
+    .catch((error)=> {
+      let errorModal = document.querySelector('#modal')
+      errorModal.classList.remove('hidden')
+      let p = document.querySelector('#modal-message')
+      p.textContent = `${error}`
+      setTimeout(function(){
+        errorModal.className = 'hidden'
+      },3000)
+      
+      return error
+    })
+    
+  }))
+  
+  
+}
+
+handlelike()
 
 
 //------------------------------------------------------------------------------
